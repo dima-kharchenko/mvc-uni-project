944dea2f8a56414368a69883e57e0602e7a53d87
dima-kharchenko <dmitriy020606@gmail.com>
2025-06-03 15:36:01 +0200

    Add login & signup error message

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,15 @@ const authController = require('../controllers/authController')
 const router = express.Router();
 
 router.post('/login', authController.login)
-router.get('/login', (_, res) => {
-    res.render('login')
+router.get('/login', (req, res) => {
+    const failed = req.query.failed
+    res.render('login', { failed: failed })
 })
 
 router.post('/signup', authController.signup)
-router.get('/signup', (_, res) => {
-    res.render('signup')
+router.get('/signup', (req, res) => {
+    const failed = req.query.failed
+    res.render('signup', { failed: failed })
 })
 
 router.post('/logout', authController.logout)
