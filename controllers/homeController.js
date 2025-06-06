dd7fd3720dbdff47c24cd05eeefc3bc52a8e0f3b
Dmytro Kharchenko <84476066+dima-kharchenko@users.noreply.github.com>
2025-06-06 13:17:28 +0200

    Main

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,12 +1,9 @@
 const Expense = require('../models/Expense');
-const { Op } = require('sequelize');
 
 exports.homeView = async (req, res) => {
     if (!req.user) { res.redirect('/auth/login') }
     const userId = req.user.id;
     const username = req.user.username;
-    const now = new Date();
-    const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
 
     try {
         const lastExpenses = await Expense.findAll({
@@ -15,27 +12,9 @@ exports.homeView = async (req, res) => {
             limit: 5,
         });        
 
-        const totalMonth = await Expense.sum('amount', {
-            where: {
-                userId,
-                date: { [Op.gte]: startOfMonth },
-            },
-        });
-
-        const totalCount = await Expense.count({
-            where: {
-                userId,
-                date: { [Op.gte]: startOfMonth },
-            },
-        });
-
         res.render('home', {
             username,
             lastExpenses,
-            stats: {
-                totalMonth: totalMonth || 0,
-                totalCount,
-            },
         });
 
     } catch (error) {
@@ -43,10 +22,6 @@ exports.homeView = async (req, res) => {
         res.render('home', {
             username,
             lastExpenses: [],
-            stats: {
-                totalMonth: 0,
-                totalCount: 0,
-            },
         });
     }
 }
